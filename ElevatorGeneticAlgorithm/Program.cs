69f527c5c7381ba53b0f8fb688c1c64f12c65a09
nenoNaninu <neno@neno-garden.com>
2019-01-06 02:33:10 +0900

    プロジェクト ファイルを追加します。

---
+++ ElevatorGeneticAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElevatorGeneticAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
