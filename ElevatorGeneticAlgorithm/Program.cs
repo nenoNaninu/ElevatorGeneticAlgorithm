0efec90e3a0a9e498bf77d5f4804ad4647ac3b57
nenoNaninu <neno@neno-garden.com>
2019-01-22 02:57:47 +0900

    refactor: インデント崩れていたので修正

--- ElevatorGeneticAlgorithm/Program.cs
+++ ElevatorGeneticAlgorithm/Program.cs
@@ -10,30 +10,30 @@ namespace ElevatorGeneticAlgorithm
     {
         static async Task Main(string[] args)
         {
-                var totalPeopleNum = Database.Configuration.TotalPeopleNumber;
-                var genericNumber = Database.Configuration.GenericNumber;
-                var pairNumber = Database.Configuration.PairNumberOfCrossoverParents;
-                var mutationRate = Database.Configuration.MutationRate;
-                var maxCarrying = Database.Configuration.MaxLoadingNum;
-                var elevatorSpeed = Database.Configuration.ElevatorSpeed;
-                var openDoorSpeed = Database.Configuration.OpenDoorTime;
-                
-                //遺伝子の配列。
-                var generics = new List<Genetic>(genericNumber);
+            var totalPeopleNum = Database.Configuration.TotalPeopleNumber;
+            var genericNumber = Database.Configuration.GenericNumber;
+            var pairNumber = Database.Configuration.PairNumberOfCrossoverParents;
+            var mutationRate = Database.Configuration.MutationRate;
+            var maxCarrying = Database.Configuration.MaxLoadingNum;
+            var elevatorSpeed = Database.Configuration.ElevatorSpeed;
+            var openDoorSpeed = Database.Configuration.OpenDoorTime;
 
-                for (int i = 0; i < totalPeopleNum; i++)
-                {
-                    generics.Add(new Genetic(totalPeopleNum));
-                }
+            //遺伝子の配列。
+            var generics = new List<Genetic>(genericNumber);
 
-                //人の配列
-                var peoples = await Database.ReadPeoples();
+            for (int i = 0; i < totalPeopleNum; i++)
+            {
+                generics.Add(new Genetic(totalPeopleNum));
+            }
 
-                //ここから評価関数
-                await GeneticAlgorithm.Learning(10, generics, peoples, pairNumber, mutationRate, genericNumber, maxCarrying, elevatorSpeed, openDoorSpeed);
+            //人の配列
+            var peoples = await Database.ReadPeoples();
 
-                Console.WriteLine("finish!");
-                Console.ReadLine();
+            //ここから評価関数
+            await GeneticAlgorithm.Learning(10, generics, peoples, pairNumber, mutationRate, genericNumber, maxCarrying, elevatorSpeed, openDoorSpeed);
+
+            Console.WriteLine("finish!");
+            Console.ReadLine();
         }
     }
 }
